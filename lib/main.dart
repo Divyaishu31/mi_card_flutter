264d93647a061fd2a991a22e849335f492915e90
Divya Agrawal <agarwaldivya3101@gmail.com>
2023-08-12 15:46:17 +0530

    How to add custom font, images to your app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  const myApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.teal.shade100,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Center(
+                child: CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/Divya.jpg'),
+                ),
+              ),
+              Text('Divya',
+                  style: TextStyle(
+                      fontFamily: 'Pacifico',
+                      fontStyle: FontStyle.normal,
+                      fontSize: 40,
+                      color: Colors.black)),
+              Text(
+                'SOFTWARE DEVELOPER',
+                style: TextStyle(
+                    fontFamily: 'SourceSans',
+                    fontSize: 10,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.normal
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
